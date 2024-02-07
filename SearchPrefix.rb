f1b6184bb752538ed8c9baf9c45bddeee057ebca
Nina Malec-Kosak <maleckosak1@kenyon.edu>
2024-02-07 00:18:39 -0500

    commitsss

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,5 +1,9 @@
 def searchPrefix(arr,s)
-
+    for i in arr
+        if i.start_with?(s)
+            puts i
+        end
+    end
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
